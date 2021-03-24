f04a87dd79462ff0d1db842f07dd839cc9d52c4c
tckthecreator <dsoouuzavitu@gmail.com>
2021-03-24 17:55:58 -0300

    quase tudo

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import perfil from './assets/perfil.jpeg'
+import Icon from 'react-native-vector-icons/Feather'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.page} >
+        <View style={styles.containerperfil} >
+          <Image source={perfil} style={styles.imageperfil} />
+          <Text style={styles.name_style}>Victor Souza</Text>
+          <Text style={styles.role}>Desenvolvedor Mobile</Text>
+          <View style={styles.containericons}>
+            <Icon style={styles.icons} name='github' size={24}/>
+            <Icon style={styles.icons} name='twitter' size={24}/>
+            <Icon style={styles.icons} name='linkedin' size={24}/>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  // Containers
+  page: {
+    backgroundColor: '#E7E7E7',
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  containerperfil: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  containericons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 0.5,
+  },
+  
+  // Componentes
+  icons: {
+    paddingHorizontal: 10.75,
+  },
+  imageperfil: {
+    width: 125,
+    height: 125,
+    borderRadius: 70,
+  },
+  name_style: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+    marginTop: 10
+  },
+  role: {
+    color: '#939393',
+    fontSize: 15,
+    fontWeight: '300',
+    marginBottom: 10,
+  },
+}); 
+
+export default App;
