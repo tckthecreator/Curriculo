fd74027e012c4ab826899319111ea7aa9425db95
tckthecreator <dsoouuzavitu@gmail.com>
2021-03-24 22:05:19 -0300

    Criando interação  com os icons da rede social

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {StyleSheet, View, Text, Image, Alert} from 'react-native';
 import perfil from './assets/perfil.jpeg'
 import Icon from 'react-native-vector-icons/Feather'
 
 const App = () => {
+  function handleicon(social_network) {
+    switch (social_network) {
+      case 'linkedin':
+        Alert.alert('Meu Linkedin', 'https://www.linkedin.com/in/victor-souza-492765204/');
+        break;
+      case 'twitter':
+        Alert.alert('Meu Twitter', 'https://twitter.com/tckthecreator');
+        break;
+      case 'github':
+          Alert.alert('Meu GitHub', 'https://github.com/tckthecreator')
+          break;
+    }
+  }
+  
   return (
     <>
       <View style={styles.page} >
@@ -12,9 +26,9 @@ const App = () => {
           <Text style={styles.name_style}>Victor Souza</Text>
           <Text style={styles.role}>Desenvolvedor Mobile</Text>
           <View style={styles.container_icons}>
-            <Icon style={styles.icons} name='github' size={24}/>
-            <Icon style={styles.icons} name='twitter' size={24}/>
-            <Icon style={styles.icons} name='linkedin' size={24}/>
+            <Icon style={styles.icons} name='github' size={24} onPress={() => handleicon('github')}/>
+            <Icon style={styles.icons} name='twitter' size={24} onPress={() => handleicon('twitter')}/>
+            <Icon style={styles.icons} name='linkedin' size={24} onPress={() => handleicon('linkedin')}/>
           </View>
         </View>
         <View style={styles.container_container_cards}>
